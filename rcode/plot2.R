357f74277f7215b9a1eaf35832c47c35fb65137a
moiz <moizmuhammad@yahoo.com>
2015-01-12 02:26:33 +0500

    first commit

---
+++ rcode/plot2.R
@@ -0,0 +1,22 @@
+library(data.table)
+
+#read the file as data.table
+dt = fread('household_power_consumption.txt')
+
+#subset data on following dates: 1st and 2nd Feb, 2007
+dts = subset(dt, Date %in% c('1/2/2007','2/2/2007'))
+
+#convert Global_active_power column to numeric
+dts$Global_active_power = as.numeric(dts$Global_active_power)
+
+#convert Date to date column
+dts$DateTime = as.POSIXct( paste(dts$Date,dts$Time),tz='GMT',format='%d/%m/%Y %H:%M:%S')
+
+#open a png file device
+png(filename='figure/plot2.png',width=480, height=480, units='px')
+
+#plot 
+plot(dts$DateTime,dts$Global_active_power, type='l', xlab='', ylab='Global Active Power (kilowatts)')
+
+#close the device
+dev.off()
