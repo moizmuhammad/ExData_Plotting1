7f1596587009dea4838f100e27d1ba04ed9d8c4e
moiz <moizmuhammad@yahoo.com>
2015-01-11 21:26:03 +0500

    now output to png file device

--- rcode/plot1.R
+++ rcode/plot1.R
@@ -9,9 +9,11 @@ dts = subset(dt, Date %in% c('1/2/2007','2/2/2007'))
 #convert Global_active_power column to numeric
 dts$Global_active_power = as.numeric(dts$Global_active_power)
 
+#open a png file device
 png(filename='figure/plot1.png',width=480, height=480, units='px')
 
 #plot histogram
 hist(dts$Global_active_power, main='Global Active Power', xlab='Global Active Power (kilowatts)', col='red')
 
+#close the device
 dev.off()
